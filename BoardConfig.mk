bc93b8e739f6efbcd3770510905e2b8a434298ab
Ash Charles <ashcharles@gmail.com>
2014-02-25 13:48:39 -0800

    Correct name of linux defconfig.
    Signed-off-by: Ash Charles <ashcharles@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_KERNEL := true
 UBOOT_CONFIG := omap4_duovero
-KERNEL_CONFIG := duovero_android
+KERNEL_CONFIG := android_4430_defconfig
 
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
